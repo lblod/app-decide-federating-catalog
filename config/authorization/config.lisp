1b161805f14f8e41f7bccec29be4385828cee2d6
onodrim <alexander.van.den.berghe@redpencil.io>
2026-06-05 18:07:06 +0200

    feat(authz): allow reading DCAT-related resources
    - Clean up the config by removing unnecessary comments
    - Enable query logging to simplify development
    - Add graph specifications and grants

--- config/authorization/config.lisp
+++ config/authorization/config.lisp
@@ -1,5 +1,3 @@
-;; NOTE (08/06/2026): This is an unmodified, except for this note, default config.  It will be
-;; edited when more functionality is added to the app, e.g. frontends.
 ;;;;;;;;;;;;;;;;;;;
 ;;; delta messenger
 (in-package :delta-messenger)
@@ -11,7 +9,7 @@
 ;;;;;;;;;;;;;;;;;
 ;;; configuration
 (in-package :client)
-(setf *log-sparql-query-roundtrip* nil) ; change nil to t for logging requests to virtuoso (and the response)
+(setf *log-sparql-query-roundtrip* t) ; change nil to t for logging requests to virtuoso (and the response)
 (setf *backend* "http://triplestore:8890/sparql")
 
 (in-package :server)
@@ -39,31 +37,62 @@
   :mu "http://mu.semte.ch/vocabularies/core/"
   :session "http://mu.semte.ch/vocabularies/session/"
   :ext "http://mu.semte.ch/vocabularies/ext/"
-  ;; Custom prefix URIs here, prefix casing is ignored
-  )
+  ;; App
+  :cms "http://mu.semte.ch/vocabulary/cms/"
+  :dcat "http://www.w3.org/ns/dcat#"
+  :dct "http://purl.org/dc/terms/"
+  :foaf "http://xmlns.com/foaf/0.1/"
+  :skos "http://www.w3.org/2004/02/skos/core#")
 
 
 ;;;;;;;;;
 ;; Graphs
-;;
-;; These are the graph specifications known in the system.  No
-;; guarantees are given as to what content is readable from a graph.  If
-;; two graphs are nearly identical and have the same name, perhaps the
-;; specifications can be folded too.  This could help when building
-;; indexes.
 
-(define-graph public ("http://mu.semte.ch/graphs/public")
-  (_ -> _)) ; public allows ANY TYPE -> ANY PREDICATE in the direction
-            ; of the arrow
+(define-graph ldes-public ("http://mu.semte.ch/graphs/ldes/decide-public")
+  ("dcat:Catalog" -> _)
+  ("dcat:Dataset" -> _)
+  ("dcat:Distribution" -> _)
+  ("dcat:CatalogRecord" -> _)
+  ("skos:Concept" -> _)
+  ("skos:ConceptScheme" -> _)
+  ("foaf:Agent" -> _)
+  ("dct:MediaTypeOrExtent" -> _)
+  ("cms:Page" -> _))
 
-;; Example:
-;; (define-graph company ("http://mu.semte.ch/graphs/companies/")
-;;   ("foaf:OnlineAccount"
-;;    -> "foaf:accountName"
-;;    -> "foaf:accountServiceHomepage")
-;;   ("foaf:Group"
-;;    -> "foaf:name"
-;;    -> "foaf:member"))
+;; NOTE (08/06/2026): The following are placeholder for intended graphs for the data consumed from
+;; the partner LDES.
+;; (define-graph ldes-bamberg ("http://mu.semte.ch/graphs/ldes/bamberg")
+;;   ("dcat:Catalog" -> _)
+;;   ("dcat:Dataset" -> _)
+;;   ("dcat:Distribution" -> _)
+;;   ("dcat:CatalogRecord" -> _)
+;;   ("skos:Concept" -> _)
+;;   ("skos:ConceptScheme" -> _)
+;;   ("foaf:Agent" -> _)
+;;   ("dct:MediaTypeOrExtent" -> _)
+;;   ("cms:Page" -> _))
+
+;; (define-graph ldes-freiburg ("http://mu.semte.ch/graphs/ldes/freiburg")
+;;   ("dcat:Catalog" -> _)
+;;   ("dcat:Dataset" -> _)
+;;   ("dcat:Distribution" -> _)
+;;   ("dcat:CatalogRecord" -> _)
+;;   ("skos:Concept" -> _)
+;;   ("skos:ConceptScheme" -> _)
+;;   ("foaf:Agent" -> _)
+;;   ("dct:MediaTypeOrExtent" -> _)
+;;   ("cms:Page" -> _))
+
+;; (define-graph ldes-ghent ("http://mu.semte.ch/graphs/ldes/ghent")
+;;   ("dcat:Catalog" -> _)
+;;   ("dcat:Dataset" -> _)
+;;   ("dcat:Distribution" -> _)
+;;   ("dcat:CatalogRecord" -> _)
+;;   ("skos:Concept" -> _)
+;;   ("skos:ConceptScheme" -> _)
+;;   ("foaf:Agent" -> _)
+;;   ("dct:MediaTypeOrExtent" -> _)
+;;   ("cms:Page" -> _))
 
 
 ;;;;;;;;;;;;;
@@ -71,19 +100,22 @@
 
 (supply-allowed-group "public")
 
-(grant (read write)
-       :to-graph public
+
+;;;;;;;;;
+;; Grants
+
+(grant (read)
+       :to-graph ldes-public
        :for-allowed-group "public")
 
-;; example:
+;; (grant (read)
+;;        :to-graph ldes-bamberg
+;;        :for-allowed-group "public")
 
-;; (supply-allowed-group "company"
-;;   :query "PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-;;           SELECT DISTINCT ?uuid WHERE {
-;;             <SESSION_ID ext:belongsToCompany/mu:uuid ?uuid
-;;           }"
-;;   :parameters ("uuid"))
+;; (grant (read)
+;;        :to-graph ldes-freiburg
+;;        :for-allowed-group "public")
 
-;; (grant (read write)
-;;        :to company
-;;        :for "company")
+;; (grant (read)
+;;        :to-graph ldes-ghent
+;;        :for-allowed-group "public")
