3cf920b6051c4acd333c07b8bb90cadde5f6a2ec
onodrim <alexander.van.den.berghe@redpencil.io>
2026-06-10 16:02:26 +0200

    refactor(delta-notifier): remove duplicate rule
    This duplicate rule was added when the cache service was added to the stack.  I
    did not notice at the time the config was already initialised with such a rule.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -14,18 +14,4 @@ export default [
       ignoreFromSelf: true,
     },
   },
-  {
-    match: {
-      subject: {},
-    },
-    callback: {
-      url: "http://resource/.mu/delta",
-      method: "POST",
-    },
-    options: {
-      resourceFormat: "v0.0.1",
-      gracePeriod: 250,
-      ignoreFromSelf: true,
-    },
-  },
 ];
