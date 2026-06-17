283582373acfa884529d9dec2ddcf194fbbd7c3e
onodrim <alexander.van.den.berghe@redpencil.io>
2026-06-17 15:27:01 +0200

    feat(ldes-client): allow setting DECIDe app feed via env var

--- config/ldes-client/config.ts
+++ config/ldes-client/config.ts
@@ -1,9 +1,9 @@
 const config = {
   endpoints: [
     {
-      name: "public",
-      LDES_BASE: "https://ds.decide-dev.s.redhost.be/ldes/public/",
-      FIRST_PAGE: "https://ds.decide-dev.s.redhost.be/ldes/public/1",
+      name: "decide-public",
+      LDES_BASE: process.env.DECIDE_LDES_BASE,
+      FIRST_PAGE: process.env.DECIDE_LDES_FIRST_PAGE,
       STATUS_GRAPH: "http://mu.semte.ch/graphs/ldes/decide-public-status",
       TARGET_GRAPH: "http://mu.semte.ch/graphs/ldes/decide-public",
       BATCH_GRAPH: "http://mu.semte.ch/graphs/ldes/decide-public-batch",
