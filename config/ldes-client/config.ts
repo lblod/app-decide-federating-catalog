6a3e424b330cb18a0322ffe328d992cfec9e70d0
onodrim <alexander.van.den.berghe@redpencil.io>
2026-08-10 16:29:41 +0200

    feat(ldes): consume feed from Freiburg

--- config/ldes-client/config.ts
+++ config/ldes-client/config.ts
@@ -11,6 +11,14 @@ const config = {
     // TODO: Configure other endpoints when available
     // See `config/authorization/config.lisp` for the intended target graphs per
     // partner.
+    {
+      name: "freiburg-public",
+      LDES_BASE: process.env.FREIBURG_LDES_BASE,
+      FIRST_PAGE: process.env.FREIBURG_LDES_FIRST_PAGE,
+      STATUS_GRAPH: "http://mu.semte.ch/graphs/ldes/freiburg/public-status",
+      TARGET_GRAPH: "http://mu.semte.ch/graphs/ldes/freiburg/public",
+      BATCH_GRAPH: "http://mu.semte.ch/graphs/ldes/freiburg/public-batch",
+    },
   ],
 };
 
