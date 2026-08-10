588a64e9ace52e46c3424354a2caed5161eac35e
onodrim <alexander.van.den.berghe@redpencil.io>
2026-08-10 16:33:59 +0200

    feat(ldes): consume feed from Bamberg

--- config/ldes-client/config.ts
+++ config/ldes-client/config.ts
@@ -19,6 +19,14 @@ const config = {
       TARGET_GRAPH: "http://mu.semte.ch/graphs/ldes/freiburg/public",
       BATCH_GRAPH: "http://mu.semte.ch/graphs/ldes/freiburg/public-batch",
     },
+    {
+      name: "bamberg-public",
+      LDES_BASE: process.env.BAMBERG_LDES_BASE,
+      FIRST_PAGE: process.env.BAMBERG_LDES_FIRST_PAGE,
+      STATUS_GRAPH: "http://mu.semte.ch/graphs/ldes/bamberg/public-status",
+      TARGET_GRAPH: "http://mu.semte.ch/graphs/ldes/bamberg/public",
+      BATCH_GRAPH: "http://mu.semte.ch/graphs/ldes/bamberg/public-batch",
+    },
   ],
 };
 
